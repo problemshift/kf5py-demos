2ca0e72b9697de8c5e1f90f489c296325d4d28c6
Chris Teplovs <chris.teplovs@gmail.com>
2014-08-15 00:37:09 -0400

    Added login.py

---
+++ login.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import kf5py
+
+conn = kf5py.connection('http://localhost:8080/kforum','you','yourpassword')
+if conn.is_valid():
+    print "Ok!"
+else:
+    print "Error!"
