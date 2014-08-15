3b2775e400a1975be185488f694634f8e6c40c2d
Chris Teplovs <chris.teplovs@gmail.com>
2014-08-15 00:39:03 -0400

    Update login.py
    Added trailing slash to endpoint.  Not strictly necessary, but omitting it will display a warning.

--- login.py
+++ login.py
@@ -2,7 +2,7 @@
 
 import kf5py
 
-conn = kf5py.connection('http://localhost:8080/kforum','you','yourpassword')
+conn = kf5py.connection('http://localhost:8080/kforum/','you','yourpassword')
 if conn.is_valid():
     print "Ok!"
 else:
